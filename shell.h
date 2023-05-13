d196a308b5d3b6f2dc748e49dcba41d851c0b261
KARIM BOUARICHA <113786802+bouarichaKarim@users.noreply.github.com>
2023-05-13 18:46:56 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -126,7 +126,7 @@ char *find_path(info_t *, char *, char *);
 int loophsh(char **);
 
 /* toem_errors.c */
-void _eputs(char *);
+void e_puts(char *);
 int _eputchar(char);
 int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
